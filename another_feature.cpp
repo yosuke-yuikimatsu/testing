0fdc129e959d2e18af6f4890d1dae5178ab5e4ab
yosuke-yuikimatsu <geyvandovdd@gmail.com>
2024-12-05 17:31:28 +0300

    bebra

--- another_feature.cpp
+++ another_feature.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 long long fib(int n) {
-    std::vector<long long> fib;
+    std::vector<long long> fib( n + 1);
     fib[0] = 1;
     fib[1] = 1;
     for (int i = 0; i <= n; ++i) {
@@ -14,5 +14,5 @@ long long fib(int n) {
 int main() {
     int n;
     std::cin >> n;
-    std::cout << fib(n)
+    std::cout << fib(n);
 }
